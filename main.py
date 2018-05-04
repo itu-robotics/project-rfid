b1f95509932fe600ff0e64c4410736113c0a454b
Unknown <sencer_yazici98@hotmail.com>
2018-05-04 16:36:55 +0300

    Message sending added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from server.socket_server import BasicChatServer
 import database.python_database.object_xml_operations as o2x
-
+import thread
 database_path = "/database/database.xml"
 
 class TCPParser(object):
@@ -12,19 +12,25 @@ class TCPParser(object):
     }
 
 
-def mes(message):
+def mes(serv, message):
     # Message will be sent to TCPParser
     # To check with dispatcher and execute function.
     # FIXME: Each function has differrent attributes, Cant execute them together.
 
     # TODO: Execute Authentication Test here. Check is user allowed
     # IDEA: Different user types to execute group of functions. Allow some users to execute some etc. remove_member
-
+    print message
+    #serv.broadcast(raw_input())
     pass
+
+def send_to_client(serv):
+    while True:
+        serv.broadcast(raw_input() + "\n")
 
 def main():
     server = BasicChatServer(mes)
-    server.run()
+    thread.start_new_thread(server.run,())
+    send_to_client(server)
 
 if __name__ == "__main__":
     main()
