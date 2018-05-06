5295714059bd38ec0407344814edaf4f6fc565de
Sencer Yazici <sencer_yazici98@hotmail.com>
2018-05-06 03:36:11 +0300

    Test Request Sender Created

---
+++ test_request.py
@@ -0,0 +1,18 @@
+import socket
+import json
+import random, string
+import time
+import sys
+host = "localhost"
+port = 9090
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+
+id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+_dict = {"id":id, "request_type":"door_clearance", "content":sys.argv[1]}
+
+client.send(json.dumps(_dict))
+print "SENDING " + json.dumps(_dict)
+print "RECEIVED " + client.recv(100).split("\n")[0]
+client.close()
