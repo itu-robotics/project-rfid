2a45cbf6f969da8ac2f8061b6a103565fb7871f7
Sencer Yazici <sencer_yazici98@hotmail.com>
2018-05-09 07:05:52 +0300

    JSON style database save/load created

--- database/json_database/json_database_operations.py
+++ database/json_database/json_database_operations.py
@@ -7,6 +7,7 @@ def _save_object(obj, name="object.pkl"):
     try:
         file = open(name, 'wb')
         pickle.dump(obj, file, pickle.HIGHEST_PROTOCOL)
+        file.close()
         return True
     except:
         return False
@@ -14,7 +15,9 @@ def _save_object(obj, name="object.pkl"):
 def _load_object(name="object.pkl"):
     try:
         file = open(name, 'rb')
-        return pickle.load(file)
+        s = pickle.load(file)
+        file.close()
+        return s
     except:
         return None
 
@@ -39,8 +42,46 @@ class PersonObjectManager():
         if not _data is None:
             self.persons =_data
 
+    def load_json(self):
+        file = open("database/database.json", 'r')
+        json_str = file.read()
+        json_str = "".join(json_str.split("\n"))
+        json_str = json_str.replace("]", "")
+        json_str = json_str.replace("[", "")
+
+        json_arr = json_str.split("||")
+        database_arr = []
+        for jstr in json_arr:
+            direc = json.loads(jstr)
+            arr = []
+            for attr in _structure:
+                val = direc[attr]
+                arr.append(val)
+            database_arr.append(Member(arr))
+        self.persons = database_arr
+
+    def save_json(self):
+        json_str = "[ "
+        count = 0
+        for member in self.persons:
+            count += 1
+            person_dir = {}
+            for attribute in _structure:
+                person_dir[attribute] = member.__getattribute__(attribute)
+            if count == len(self.persons):
+                json_str += json.dumps(person_dir)
+            else:
+                json_str += json.dumps(person_dir) + "|| \n"
+
+        json_str += " ]"
+        file = open("database/database.json", 'w')
+        file.write(json_str)
+        file.close()
+
+
     def save(self):
         _save_object(self.persons, self.path)
+        self.save_json()
 
     def addS(self, string_to_parse, delimeter=","):
         self.persons.append(Member(string_to_parse.split(delimeter)))
