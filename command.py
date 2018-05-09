39c7eec9c566ec9dc84a25ea969aabe392df17dd
Sencer Yazici <sencer_yazici98@hotmail.com>
2018-05-09 07:05:30 +0300

    More listing options added to the command

--- command.py
+++ command.py
@@ -5,12 +5,23 @@ manager = PersonObjectManager()
 manager.load()
 
 input = raw_input("Database Terminal\n-$ ")
-
+selected_person = None
 while input != "exit":
-    if input == "ls":
-        list = manager.find_all("", "")
-        for l in list:
-            print l.serialize()
+    if "ls" in input.split(" ")[0]:
+        args = input.split(" ")
+        list = None
+        status = True
+        if len(args) < 2:
+            list = manager.find_all("", "")
+        elif len(args) > 2:
+            list = manager.find_all(args[2], args[1])
+        elif len(args) == 4:
+            pass
+        else:
+            status = False
+        if status:
+            for l in list:
+                print l.serialize()
     elif input == "add":
         manager.add()
     elif "rm -rf" in input:
@@ -19,4 +30,11 @@ while input != "exit":
     elif input == "add -s":
         _s = raw_input("Enter string\n")
         manager.addS(_s)
+    elif input == "debug":
+        manager.load_json()
+    elif input == "save":
+        manager.save()
+    elif input.split(" ")[0] == "cd":
+        selected_person = manager.find_all(input.split(" ")[1], "id")[0]
+        print selected_person.serialize()
     input = raw_input("-$ ")
